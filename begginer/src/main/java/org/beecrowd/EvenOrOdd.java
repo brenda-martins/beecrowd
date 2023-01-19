5dcf52d0346dd8a63a7f051cbc61392cc1751257
Brenda Martins <brendamichele16@hotmail.com>
2023-01-19 06:07:58 -0300

    Added description

--- begginer/src/main/java/org/beecrowd/EvenOrOdd.java
+++ begginer/src/main/java/org/beecrowd/EvenOrOdd.java
@@ -2,6 +2,17 @@ package org.beecrowd;
 
 import java.util.Scanner;
 
+//Read an integer value N. After, read these N values and print a message for each value saying if this
+// value is odd, even, positive or negative. In case of zero (0), although the correct description would be "EVEN NULL",
+// because by definition zero is even, your program must print only "NULL", without quotes.
+//        Input
+//
+//        The first line of input is an integer N (N < 10000), that indicates the total number of test cases.
+//        Each case is a integer number X (-107 < X <107)..
+//        Output
+//
+//        For each test case, print a corresponding message, according to the below example.
+//        All messages must be printed in uppercase letters and always will have one space between two words in the same line.
 public class EvenOrOdd {
 
     public static void main(String[] args) {
